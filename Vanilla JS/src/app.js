d49ea65fab3298b42e4d416580f288cc267edb48
markuskreukniet <shonof@gmail.com>
2022-09-09 22:50:46 +0200

    app comp improvement

--- Vanilla JS/src/app.js
+++ Vanilla JS/src/app.js
@@ -1,10 +1,29 @@
+const scriptUrls = [
+  "./src/components/LabeledNumberInput.js",
+  "./src/components/ReverbCalculatorApp.js",
+];
+
+function includeScriptInHead(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+
+    script.setAttribute("src", src);
+    script.addEventListener("load", resolve);
+    script.addEventListener("error", reject);
+
+    document.head.appendChild(script);
+  });
+}
+
 function App(parent) {
   let that = this;
   this.parent = parent;
 
   this.fetch = async function () {
     try {
-      await includeScriptInHead("./src/components/ReverbCalculatorApp.js");
+      for (let url of scriptUrls) {
+        await includeScriptInHead(url);
+      }
     } catch (e) {
       console.log(e);
     }
@@ -19,17 +38,5 @@ function App(parent) {
   this.init();
 }
 
-function includeScriptInHead(src) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement("script");
-
-    script.setAttribute("src", src);
-    script.addEventListener("load", resolve);
-    script.addEventListener("error", reject);
-
-    document.head.appendChild(script);
-  });
-}
-
-let app = document.querySelector("#app");
+const app = document.querySelector("#app");
 App(app);

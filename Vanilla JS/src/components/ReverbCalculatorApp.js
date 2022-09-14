cca65f94278a26c3d7c4e37b968b60dfcfc4e924
markuskreukniet <shonof@gmail.com>
2022-09-14 13:35:46 +0200

    WIP

--- Vanilla JS/src/components/ReverbCalculatorApp.js
+++ Vanilla JS/src/components/ReverbCalculatorApp.js
@@ -24,6 +24,8 @@ function ReverbCalculatorApp(parent) {
   this.tempo = 128;
   this.durationResult = textValueCombinations[0].value;
   this.valueOne = defaultValueSelect;
+  this.valueTwo = defaultValueSelect;
+  this.subtractionTextColon = preDelayColon;
 
   function addColon(string) {
     return `${string}:`;
@@ -32,16 +34,31 @@ function ReverbCalculatorApp(parent) {
   // TODO: these change functions same as React project
   function tempoChange(e) {
     console.log("tempoChange", e.target.value);
+    that.tempo = e.target.value;
     that.render();
   }
 
   function durationResultChange(e) {
     console.log("durationResultChange", e.target.value);
+    that.durationResult = e.target.value;
     that.render();
   }
 
   function valueOneChange(e) {
     console.log("valueOneChange", e.target.value);
+    that.valueOne = e.target.value;
+    that.render();
+  }
+
+  function valueTwoChange(e) {
+    console.log("valueTwoChange", e.target.value);
+    that.valueTwo = e.target.value;
+    that.render();
+  }
+
+  function subtractionTextColonChange(e) {
+    console.log("subtractionTextColonChange", e.target.value);
+    that.subtractionTextColon = e.target.value;
     that.render();
   }
 
@@ -84,10 +101,37 @@ function ReverbCalculatorApp(parent) {
 
       p.innerHTML = "+";
       that.divRow.appendChild(p);
+
+      new LabeledTextSelect(
+        that.divRow,
+        durations,
+        that.valueTwo,
+        decayColon,
+        valueTwoChange
+      ); // TODO: className
     } else {
+      new LabeledTextSelect(
+        that.divRow,
+        durations,
+        that.valueOne,
+        totalReverbColon,
+        valueOneChange
+      ); // TODO: className
+
       p.innerHTML = "-";
       that.divRow.appendChild(p);
+
+      new TextSelect(
+        that.divRow,
+        values,
+        that.subtractionTextColon,
+        subtractionTextColonChange
+      ); // TODO: className
+
+      new TextSelect(that.divRow, durations, that.valueTwo, valueTwoChange); // TODO: className
     }
+
+    //
   };
   this.init = function () {
     that.create();

069ad5d8d0bc4fa88f9eb0f939d4e3b5ae85af60
markuskreukniet <shonof@gmail.com>
2022-10-24 10:53:20 +0200

    added durations

--- Vanilla JS/src/constants/durations.constants.js
+++ Vanilla JS/src/constants/durations.constants.js
@@ -24,6 +24,46 @@ const durations = [
   "1/32 note triplet",
   "1/32 note",
   "1/32 note dotted",
+
+  "1/16 note triplet",
+  "1/16 note",
+  "1/16 note dotted",
+
+  "1/8 note triplet",
+  "1/8 note",
+  "1/8 note dotted",
+
+  "1/4 note triplet",
+  "1/4 note",
+  "1/4 note dotted",
+
+  "1/2 note triplet",
+  "1/2 note",
+  "1/2 note dotted",
+
+  "1 note triplet",
+  "1 note",
+  "1 note dotted",
+
+  "2 notes triplet",
+  "2 notes",
+  "2 notes dotted",
+
+  "4 notes triplet",
+  "4 notes",
+  "4 notes dotted",
+
+  "8 notes triplet",
+  "8 notes",
+  "8 notes dotted",
+
+  "16 notes triplet",
+  "16 notes",
+  "16 notes dotted",
+
+  "32 notes triplet",
+  "32 notes",
+  "32 notes dotted",
 ];
 
 export default durations;

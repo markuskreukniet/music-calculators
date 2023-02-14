0f30fe15b9334035a10f7aa806818215e3c88e88
markuskreukniet <shonof@gmail.com>
2023-02-14 20:40:14 +0100

    refactor

--- Svelte/src/lib/components/RadioGroup.svelte
+++ Svelte/src/lib/components/RadioGroup.svelte
@@ -2,32 +2,27 @@
   export let textValueCombinations;
   export let value;
 
-  let name = "";
-  const length = textValueCombinations.length;
+  let name = textValueCombinations?.[0]?.value;
 
-  if (length > 0) {
-    name = textValueCombinations[0].value;
-  }
-
-  if (length > 1) {
-    for (let i = 1; i < length; i++) {
-      name += `, ${textValueCombinations[i].value}`;
-    }
+  for (let i = 1; i < textValueCombinations?.length; i++) {
+    name += `, ${textValueCombinations[i].value}`;
   }
 </script>
 
 <div>
-  {#each textValueCombinations as combination}
-    <label class="display-block">
-      <!-- on:change|preventDefault is not needed -->
-      <input
-        class="input-radio"
-        type="radio"
-        bind:group={value}
-        {name}
-        value={combination.value}
-      />
-      {combination.text}
-    </label>
-  {/each}
+  {#if textValueCombinations?.length > 0}
+    {#each textValueCombinations as combination}
+      <label class="display-block">
+        <!-- on:change|preventDefault is not needed -->
+        <input
+          class="input-radio"
+          type="radio"
+          bind:group={value}
+          {name}
+          value={combination.value}
+        />
+        {combination.text}
+      </label>
+    {/each}
+  {/if}
 </div>

3a5c7f49904f1547c52893da59b200f81ac71057
markuskreukniet <shonof@gmail.com>
2023-02-24 15:25:00 +0100

    WIP

---
+++ Vue 3 (composition and SFC)/src/components/TextSelect.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  value: {
+    type: String,
+    required: true
+  },
+  values: {
+    type: Array,
+    required: true
+  },
+  className: {
+    type: String,
+    required: false
+  }
+})
+
+const className = "select";
+
+function handleChange(e) {
+  value = e.target.value
+}
+</script>
+
+<template>
+  <select :value="value" @change.self.prevent="handleChange">
+    <option v-for="value in values" :key="value"></option>
+  </select>
+</template>

32220fd5871fa3cd2db1da4784d41c302dc5d18f
markuskreukniet <shonof@gmail.com>
2023-03-01 15:43:40 +0100

    WIP

---
+++ Vue 3 (composition and SFC)/src/components/LabeledTextSelect.vue
@@ -0,0 +1,35 @@
+<script setup>
+import TextSelect from './TextSelect.vue';
+
+const emit = defineEmits(['value']);
+
+const props = defineProps({
+  labelText: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  values: {
+    type: Array,
+    required: true
+  },
+  className: {
+    type: String,
+    required: false
+  }
+});
+
+function handleChange(e) {
+  emit('value', e);
+}
+</script>
+
+<template>
+  <label :class="className">
+    {{ labelText }}
+    <TextSelect :values="values" :value="value" @value="handleChange" />
+  </label>
+</template>
--- Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
+++ Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TextSelect from './TextSelect.vue'
+import LabeledTextSelect from './LabeledTextSelect.vue';
+import TextSelect from './TextSelect.vue';
 
+const labelText = "labelText"
 const values = ["value 1", "value 2"];
 const value = "value 2";
-
 const className = "testClass";
 
 function handleChange(e) {
@@ -12,5 +13,7 @@ function handleChange(e) {
 </script>
 
 <template>
+  <LabeledTextSelect :labelText="labelText" :values="values" :value="value" @value="handleChange"
+    :className="className" />
   <TextSelect :values="values" :value="value" @value="handleChange" :className="className" />
 </template>

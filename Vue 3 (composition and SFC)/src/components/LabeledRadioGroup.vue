2da2b6a8d27a657cec8ffa8d8b92ea5ac34b0d85
markuskreukniet <shonof@gmail.com>
2023-03-02 13:44:32 +0100

    WIP

---
+++ Vue 3 (composition and SFC)/src/components/LabeledRadioGroup.vue
@@ -0,0 +1,31 @@
+<script setup>
+import RadioGroup from './RadioGroup.vue';
+
+const emit = defineEmits(['value']);
+
+const props = defineProps({
+  labelText: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  textValueCombinations: {
+    type: Array,
+    required: true
+  }
+});
+
+function handleChange(e) {
+  emit('value', e);
+}
+</script>
+
+<template>
+  <div className="display-block padding border-bottom">
+    <p className="text-before-list">{{ props.labelText }}</p>
+    <RadioGroup :textValueCombinations="textValueCombinations" :value="value" @value="handleChange" />
+  </div>
+</template>
---
+++ Vue 3 (composition and SFC)/src/components/RadioGroup.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  textValueCombinations: {
+    type: Array,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  }
+});
+
+const name = computed(() => {
+  let name = props.textValueCombinations?.[0]?.value;
+
+  for (let i = 1; i < props.textValueCombinations?.length; i++) {
+    name += `, ${props.textValueCombinations[i].value}`;
+  }
+
+  return name;
+});
+
+</script>
+
+<!-- TODO: whole component is not completed -->
+<template>
+  <label v-for="combination in textValueCombinations" :key="combination.value" class="display-block">
+    <input type="radio" :name="name" :value="combination.value" class="input-radio" />
+    {{ combination.value }}
+  </label>
+</template>

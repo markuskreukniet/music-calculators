6494ee149766961b2ae443da0c1aa53bcc5c91ef
markuskreukniet <shonof@gmail.com>
2023-03-14 14:31:33 +0100

    WIP

--- Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
+++ Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
@@ -1,10 +1,36 @@
 <script setup>
 import { ref } from 'vue'
+import arithmeticOperation from '../constants/arithmeticOperation.constants.js';
+import durations from '../constants/durations.constants.js';
+import reverb from '../constants/reverb.constants.js';
 import LabeledTextSelect from './LabeledTextSelect.vue';
 import TextSelect from './TextSelect.vue';
 
+//
+const preDelayColon = addColon(reverb.preDelay);
+const decayColon = addColon(reverb.decay);
+const totalReverbColon = addColon(reverb.totalReverb);
+
+const values = [preDelayColon, decayColon];
+const textValueCombinations = [
+  {
+    text: 'The total reverb duration by choosing a duration for the pre-delay and decay',
+    value: arithmeticOperation.addition,
+  },
+  {
+    text: 'The pre-delay or decay duration by choosing a duration for the total reverb, and pre-delay or decay',
+    value: arithmeticOperation.subtraction,
+  },
+];
+
+const defaultValueSelect = '1/64 note';
+
+function addColon(string) {
+  return `${string}:`;
+}
+//
+
 const labelText = "labelText";
-const values = ["value 1", "value 2"];
 const value = "value 2";
 
 const className = ref('testClass');

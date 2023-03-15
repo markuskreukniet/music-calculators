98b9b97aa1fd420dbdc160823bd8f894c063e38e
markuskreukniet <shonof@gmail.com>
2023-03-15 23:34:51 +0100

    WIP

--- Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
+++ Vue 3 (composition and SFC)/src/components/ReverbCalculatorApp.vue
@@ -8,7 +8,6 @@ import LabeledRadioGroup from './LabeledRadioGroup.vue';
 import LabeledTextSelect from './LabeledTextSelect.vue';
 import TextSelect from './TextSelect.vue';
 
-//
 const preDelayColon = addColon(reverb.preDelay);
 const decayColon = addColon(reverb.decay);
 const totalReverbColon = addColon(reverb.totalReverb);
@@ -33,6 +32,9 @@ const valueTwo = defaultValueSelect;
 const durationResult = textValueCombinations[0].value;
 const tempo = 128;
 
+const child1MarginOnlyLeft = "child-1-margin-only-left-1";
+const marginOnlyLeft = "margin-only-left-1";
+
 function addColon(string) {
   return `${string}:`;
 }
@@ -43,6 +45,14 @@ function handleChangeTempo(e) {
 
 function handleChangeDurationResult(e) {
   durationResult = e;
+}
+
+function handleChangeValueOne(e) {
+  valueOne = e.detail;
+}
+
+function handleChangeValueTwo(e) {
+  valueTwo = e.detail;
 }
 //
 
@@ -58,18 +68,20 @@ function handleChange(e) {
 }
 </script>
 
-<!-- TODO: two way bind is not needed on many places -->
-
 <template>
   <LabeledNumberInput labelText="Tempo in BPM:" :value="tempo" @value="handleChangeTempo" />
   <LabeledRadioGroup labelText="Calculate in ms:" :textValueCombinations="textValueCombinations" :value="durationResult"
     @value="handleChangeDurationResult" class="display-block padding border-bottom" />
   <div class="display-flex align-items-center padding border-bottom">
     <template v-if="durationResult === arithmeticOperation.addition">
-
+      <LabeledTextSelect :labelText="preDelayColon" :values="durations" :value="valueOne" @value="handleChangeValueOne"
+        :className="child1MarginOnlyLeft" />
+      <p :class="marginOnlyLeft">+</p>
+      <LabeledTextSelect :labelText="decayColon" :values="durations" :value="valueTwo" @value="handleChangeValueTwo"
+        :class="`${child1MarginOnlyLeft} ${marginOnlyLeft}`" />
     </template>
     <template v-else>
-
+      <p :class="marginOnlyLeft">-</p>
     </template>
   </div>
   <!-- -->

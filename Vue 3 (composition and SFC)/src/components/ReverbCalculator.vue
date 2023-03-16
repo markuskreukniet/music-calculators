fdd5391b9b72375d76293d7fd7ea32897693cf1c
markuskreukniet <shonof@gmail.com>
2023-03-16 17:12:00 +0100

    WIP

--- Vue 3 (composition and SFC)/src/components/ReverbCalculator.vue
+++ Vue 3 (composition and SFC)/src/components/ReverbCalculator.vue
@@ -1,4 +1,28 @@
 <script setup>
+import MusicCalculator from './MusicCalculator.vue';
+
+const props = defineProps({
+  calculatorOperation: {
+    type: Array,
+    required: true
+  },
+  tempo: {
+    type: String,
+    required: true
+  },
+  valueOne: {
+    type: String,
+    required: true
+  },
+  valueTwo: {
+    type: String,
+    required: true
+  },
+  subtractionText: {
+    type: String,
+    required: true
+  }
+});
 
 const preDelay = addParentheses(reverb.preDelay);
 const decay = addParentheses(reverb.decay);
@@ -7,6 +31,33 @@ const totalReverb = addParentheses(reverb.totalReverb);
 function addParentheses(string) {
   return `(${string})`;
 }
+
+function getLabeledFormula() {
+  const labeledFormula = {
+    operator: props.calculatorOperation,
+    operandLabelCombinations: [
+      { operand: props.valueOne, label: preDelay },
+      { operand: props.valueTwo, label: decay },
+    ],
+    label: totalReverb,
+  };
+
+  if (props.calculatorOperation === arithmeticOperation.subtraction) {
+    labeledFormula.operandLabelCombinations[0].label = totalReverb;
+
+    if (props.subtractionText === reverb.preDelay) {
+      labeledFormula.operandLabelCombinations[1].label = preDelay;
+      labeledFormula.label = decay;
+    } else {
+      labeledFormula.operandLabelCombinations[1].label = decay;
+      labeledFormula.label = preDelay;
+    }
+  }
+
+  return labeledFormula;
+}
 </script>
 
-<template></template>
+<template>
+  <MusicCalculator :tempo="tempo" />
+</template>

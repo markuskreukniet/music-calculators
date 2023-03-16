9a41b76fcafec4bc85e11fa38ecf572f511a2fd8
markuskreukniet <shonof@gmail.com>
2023-03-16 11:48:32 +0100

    WIP

--- Vue 3 (composition and SFC)/src/components/ReverbCalculator.vue
+++ Vue 3 (composition and SFC)/src/components/ReverbCalculator.vue
@@ -1,5 +1,12 @@
 <script setup>
 
+const preDelay = addParentheses(reverb.preDelay);
+const decay = addParentheses(reverb.decay);
+const totalReverb = addParentheses(reverb.totalReverb);
+
+function addParentheses(string) {
+  return `(${string})`;
+}
 </script>
 
 <template></template>

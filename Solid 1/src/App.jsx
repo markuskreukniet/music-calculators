4855ab4fc81cf70625f387b4325f191c27546209
markuskreukniet <shonof@gmail.com>
2023-04-02 12:20:33 +0200

    WIP

--- Solid 1/src/App.jsx
+++ Solid 1/src/App.jsx
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+import { createSignal } from "solid-js";
+
 import LabeledNumberInput from "./components/LabeledNumberInput";
+import TextSelect from "./components/TextSelect";
 
 function App() {
+  const [value, setValue] = createSignal("1/64 note");
+
+  const durations = [
+    "0 ms",
+    "0.1 ms",
+    "1 ms",
+    "10 ms",
+    "30 ms",
+  ];
+
+  // onChange instead of onValue, also in other projects
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -22,6 +37,8 @@ function App() {
       </header>
 
       <LabeledNumberInput value="testValue" />
+
+      <TextSelect values={durations} value={value()} />
     </div>
   );
 }
--- Solid 1/src/components/TextSelect.jsx
+++ Solid 1/src/components/TextSelect.jsx
@@ -1,13 +1,12 @@
 export default function TextSelect(props) {
   // TODO: React project has useless this.className = "select";, and maybe also other projects
-  // TODO: value onChange
   let selectClassName = "select";
   if (props.className) {
     selectClassName += ` ${props.className}`;
   }
   
   return (
-    <select value={props.value} class={selectClassName}>
+    <select value={props.value} class={selectClassName} onChange={props.onValue}>
       <For each={props.values}>{(value, i) =>
         <option value={value}>
           {value}
